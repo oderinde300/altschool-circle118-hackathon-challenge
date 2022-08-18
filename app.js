8ad2f138313e7077476f6309ba9833be9ad4464e
Oderinde Emmanuel Oluwatobiloba <oderinde.oluwatobiloba@gmail.com>
2022-08-18 23:06:02 +0100

    last commit by emmanuel
    made changes to the responsiveness and the JavaScript logic

--- app.js
+++ app.js
@@ -1,76 +1,86 @@
-function Redactre(text_to_redact, word_to_redact, scrambler) {
+const inputToScramble = document.querySelector('#redact_text');
+const wordsToScramble = document.querySelector('#redact_words');
+const charToScrambleWith = document.querySelector('#scrambler');
+const redacButton = document.querySelector('#redact_button');
 
+const scrambleOutput = document.querySelector('#redact_output');
+
+const numberOfWordsMatched = document.querySelector('#words_Matched');
+const numberOfCharScrambled = document.querySelector('#characters_matched');
+const numberOfWordsScanned = document.querySelector('#num_of_words');
+
+const timeTaken = document.querySelector('#time')
+
+
+// The Redacter Function
+const redacWords = (sentence, words, charToRedacWith) => {
     var startTime = performance.now()
-    //Set a default value for scrambler
-    if ((scrambler == "") || (scrambler == "")) {
-        scrambler = "*";
-    } else if (word_to_redact == null) {
-        alert("ENTER A VALID WORD TO REDACT FORM YOUR POST")
-        return;
-    }
-
-    //convert all inputs to lower case
-    text_to_redact = text_to_redact.toLowerCase();
-    word_to_redact = word_to_redact.toLowerCase();
-    scrambler = scrambler.toLowerCase();
-
-    //convert sentence and words to redacts into arrays
-    let myArrayOfText = text_to_redact.split(' ');
-    let myArrayOfWords = word_to_redact.split(' ');
-    let words_Matched = 0;
+    let sentenceListArray = sentence.split(" ");
+    let wordsListArray = words.split(" ");
+    let result = '';
+    let matchedWordsToBeScrambled = [];
+    let noOfRedacWordsInput = 0;
+    let wordsMatched = 0;
     let no_of_characters = 0;
 
-    myArrayOfText.forEach(word => {
-        if (myArrayOfText.includes(word) && myArrayOfWords.includes(word)) {
-            let indexOfWord = myArrayOfText.indexOf(word);
-            myArrayOfText.splice(indexOfWord, 1, `${scrambler.repeat(word.length)}`);
-            no_of_characters += word.length;
-            words_Matched++;
+    //Checking If a Text To Be Redacted was entered
+    if (!sentence) {
+        alert("ENTER A TEXT TO REDACT")
+    }
+
+    //Checking If a Word To Be Redacted in The Text Was Entered 
+    else if (words.trim(" ").length === 0) {
+        alert("ENTER A WORD OR WORDS IN THE TEXT TO REDACT")
+    }
+
+    for (let i = 0; i < sentenceListArray.length; i++) {
+        for (let j = 0; j < wordsListArray.length; j++) {
+            if (sentenceListArray[i].toLowerCase() === wordsListArray[j].toLowerCase()) {
+                noOfRedacWordsInput += 1
+                sentenceListArray[i] = wordsListArray[j].replace(/./g, charToRedacWith || '*');
+                matchedWordsToBeScrambled.push(sentenceListArray[i])
+                no_of_characters += sentenceListArray[i].length
+                wordsMatched++
+            }
         }
-    });
+    }
+    result = sentenceListArray.join(" ");
+    console.log(matchedWordsToBeScrambled)
+
     var endTime = performance.now()
+    return {
+        result,
+        noOfRedacWordsInput,
+        noOfWordsScanned: sentenceListArray.length,
+        howLongItTook: (endTime - startTime).toFixed(5),
+        wordsMatched,
+        no_of_characters
+    };
 
-    //get number of words searched and convert array to full sentence
-    let no_Of_Words = myArrayOfText.length;
-    let full_Text = myArrayOfText.join(' ');
-    let performanceTime = endTime - startTime;
-
-
-    //push data to front-end
-    document.getElementById("redact_output").value = full_Text;
-    document.getElementById("num_of_words").innerText = no_Of_Words;
-    document.getElementById("words_Matched").innerText = words_Matched;
-    document.getElementById("characters_matched").innerText = no_of_characters;
-    document.getElementById("time").innerText = performanceTime;
-
-
-    //console.log(full_Text);
-    //console.log(no_Of_Words)
-    //console.log(`${} words scanned`)
-}
-
-
-let redact_text = ''
-let redact_words = ''
-
-window.onload = function () {
-    let redact_button = document.getElementById("redact_button");
-    let redact_text = document.getElementById("redact_text");
-    //let redact_output = document.getElementById("redact_output");
-    let redact_words = document.getElementById("redact_words");
-    let scrambler = document.getElementById("scrambler");
-    //let num_of_words = document.getElementById("num_of_words");
-    let scrambler_value = ''
-    let redact_text_value = '';
-    let redact_words_value = '';
-
-    addEventListener('input', (Event) => {
-        scrambler_value = scrambler.value;
-        redact_text_value = redact_text.value;
-        redact_words_value = redact_words.value;
-    });
-
-    redact_button.addEventListener('click', function () {
-        Redactre(redact_text_value, redact_words_value, scrambler_value);
-    });
 };
+
+redacButton.addEventListener('click', (event) => {
+    const inputToScramble1 = inputToScramble.value;
+    const wordsToScramble1 = wordsToScramble.value;
+    const charToScrambleWith1 = charToScrambleWith.value;
+
+    const output = redacWords(inputToScramble1, wordsToScramble1, charToScrambleWith1)
+    const { result, noOfWordsScanned, howLongItTook, wordsMatched, no_of_characters }
+        = output;
+
+    //Rendering Result To The DOM
+    scrambleOutput.innerText = result;
+    numberOfWordsMatched.innerHTML = wordsMatched;
+    numberOfCharScrambled.innerHTML = no_of_characters;
+    numberOfWordsScanned.innerHTML = noOfWordsScanned;
+    timeTaken.innerText = howLongItTook;
+
+    //Clear Entered Input
+    inputToScramble.value = '';
+    wordsToScramble.value = '';
+    charToScrambleWith.value = '';
+});
+
+
+
+
